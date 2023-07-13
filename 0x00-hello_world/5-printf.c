5b1737c8bfcf9081ff6ae9268ad7bf3e1f25a48b
Chinenye Akukalia <sonya12slyvia@gmail.com>
2023-07-13 11:02:27 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A programme which prints a line using printf function
+ * Return: 0 (Acheived)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n")
+	return (0);
+}
