e8a8e99f9dc78d38ce299a69deba2283bf385c98
Chinenye Akukalia <sonya12slyvia@gmail.com>
2023-07-13 13:32:43 -0700

    6-size.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A C programme that prints a line to the standard error
+ * Return 1 (Acheived)
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2019:10.19\n", stdout);
+return (1);
