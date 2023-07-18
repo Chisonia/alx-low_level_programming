a5faf17be04500776ad66c836142b96d6ad9ea6c
Chinenye Akukalia <sonya12slyvia@gmail.com>
2023-07-18 06:35:19 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_alphabet - Make the alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+
+	char c;
+
+	for (c = '0'; c <= '0'; c++)
+
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
