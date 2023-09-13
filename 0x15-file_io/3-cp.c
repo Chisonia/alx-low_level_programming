05e89b9db8b511972edb89adb44e03d69048a43a
Chinenye Akukalia <sonya12slyvia@gmail.com>
2023-09-13 05:16:07 -0700

    Update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,11 +6,12 @@
  *
  * @exit_code: The exit code to use when exiting the program.
  * @exit_message: The error message to print.
+ * @arg: Argument to insert into the error message.
  */
 
-void error_handler(int exit_code, const char *exit_message)
+void error_handler(int exit_code, const char *exit_message, const char *arg)
 {
-	dprintf(STDERR_FILENO, "%s\n", exit_message);
+	dprintf(STDERR_FILENO, exit_message, arg);
 	exit(exit_code);
 }
 
@@ -33,7 +34,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		error_handler(97, "Usage: cp file_from file_to");
+		error_handler(97, "Usage: cp file_from file_to\n", "");
 	}
 	file_from = argv[1];
 	file_to = argv[2];
@@ -41,28 +42,32 @@ int main(int argc, char *argv[])
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 	{
-		error_handler(98, "Error: Can't read from file NAME_OF_THE_FILE");
+		error_handler(98, "Error: Can't read from file %s\n", file_from);
 	}
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		error_handler(99, "Error: Can't write to NAME_OF_THE_FILE");
+		error_handler(99, "Error: Can't write to %s\n", file_to);
 	}
 	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written == -1)
 		{
-			error_handler(99, "Error: Can't write to NAME_OF_THE_FILE");
+			error_handler(99, "Error: Can't write to %s\n", file_to);
 		}
 	}
 		if (bytes_read == -1)
 		{
-			error_handler(99, "Error: Can't read from file NAME_OF_THE-FILE");
+			error_handler(98, "Error: Can't read from file %s\n", file_from);
 		}
 		if (close(fd_from) == -1 || close(fd_to) == -1)
 		{
-			error_handler(100, "Error: Can't close fd FD_VALUE");
+			error_handler(100, "Error: Can't close fd\n", "");
+		}
+			if (chmod(file_to, 0664) == -1)
+			{
+				error_handler(100, "Error: Can't set permissions on %s\n", file_to);
 		}
 		return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,6 +12,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void error_handler(int exit_code, const char *exit_message);
+void error_handler(int exit_code, const char *exit_message, const char *arg);
 
 #endif
