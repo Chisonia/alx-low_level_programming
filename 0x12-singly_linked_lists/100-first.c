481a15fa97a1f33e774b6bf24b7c9ef0e5323743
Chinenye Akukalia <sonya12slyvia@gmail.com>
2023-09-12 18:13:22 -0700

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,8 +9,9 @@
  *
  * Return: Always 0
  */
-void print_before(void)
+void print_before(void) __attribute__((constructor));
 
+void print_before(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 
