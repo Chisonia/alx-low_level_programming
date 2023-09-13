6d27bb24a5a41b4fe291f76b6db2ef9311d8966d
Chinenye Akukalia <sonya12slyvia@gmail.com>
2023-09-12 19:31:20 -0700

    update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,83 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/stat.h>
+#include"main.h"
 
 /**
- * main - Copies the contents of a file to another file.
+ * error_handler - Print an error message to the standard error streeam
+ * and exit the program with a specified exit code.
  *
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments
- *
- * Return: 0 on success.
- *
- * Description: if the argument count is not correct - exit with code 97.
- * if file_from does not exist or cannot be opened or read - exit with code 98.
- * if file_to cannot be created or written to - exit the program with 99.
- * if file_to or file-from cannot be closed - exit the program with code 100.
- *
- *
+ * @exit_code: The exit code to use when exiting the program.
+ * @exit_message: The error message to print.
  */
 
-int main(int argc, char *argv[])
+void error_handler(int exit_code, const char *exit_message)
 {
-	const char *file_from;
-	const char *file_to;
-	int source_fd;
-	int dest_fd;
-	char buffer[1024];
-	ssize_t bytes_read, bytes_written;
-
-	if (argc != 3)
-	{
-		fprintf(stderr, "Usuage: %s file_from file_to\n", argv[0]);
-		return (97);
-	}
-
-	file_from = argv[1];
-	file_to = argv[2];
-
-	source_fd = open(file_from, O_RDONLY);
-
-	if (source_fd == -1)
-	{
-		fprintf(stderr, "Error: Can't read from file %s\n", file_from);
-		return (98);
-	}
-
-	dest_fd = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-
-	if (dest_fd == -1)
-	{
-		fprintf(stderr, "Error: Can't write to file %s\n", file_to);
-		close(source_fd);
-		return (99);
-	}
-
-	while ((bytes_read = read(source_fd, buffer, sizeof(buffer))) > 0)
-	{
-		bytes_written = write(dest_fd, buffer, bytes_read);
-
-		if (bytes_written == -1)
-		{
-			fprintf(stderr, "Error: Can't write to file %s\n", file_to);
-			close(source_fd);
-			close(dest_fd);
-			return (99);
-		}
-	}
-
-	if (bytes_read == -1)
-	{
-		fprintf(stderr, "Error: Can't read from file %s\n", file_from);
-		close(source_fd);
-		close(dest_fd);
-		return (98);
-	}
-
-	close(source_fd);
-	close(dest_fd);
-
-	return (0);
+	dprintf(STDERR_FILENO, "%s\n", exit_message);
+	exit(exit_code);
 }
