5c21611c1acd4a1c8f9b11e0726e1b9ae58c2d17
chisonia <sonya12slyvia@gmail.com>
2024-07-31 15:44:35 +0100

    Search

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,17 @@
+
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/*Header*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/* Prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+/*Helper function*/
+void print_array(int *array, int left, int right);
+#endif
